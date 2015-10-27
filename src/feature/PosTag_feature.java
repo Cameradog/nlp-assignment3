36f5b819c3ef969dba89342f1d068a87e1b04c67
Cameradog <kay821221@gmail.com>
2015-10-27 20:37:52 +0800

    add Postag_feature and modify main file. Run with unprocess tagging(Words_XX)

--- src/feature/PosTag_feature.java
+++ src/feature/PosTag_feature.java
@@ -2,8 +2,19 @@ package feature;
 
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
+
+
 public class PosTag_feature extends Feature{
+	
+	MaxentTagger tagger = new MaxentTagger("models/english-left3words-distsim.tagger");
+	String posTag;
+	
 	public void execute(){
-		MaxentTagger tagger = new MaxentTagger("models/english-left3words-distsim.tagger");
+		posTag = tagger.tagTokenizedString(word); 
+		
+	}
+	
+	public String getPosTag(){
+		return posTag;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import data.Field;
 import data.FieldService;
 import feature.Capitalize_feature;
 import feature.ExampleFeature;
+import feature.PosTag_feature;
 import fileService.FileInput;
 /**
  * 
@@ -43,10 +44,13 @@ public class Main {
 	public void training(){
 		//ExampleFeature ef = new ExampleFeature();
 		Capitalize_feature cf = new Capitalize_feature();
+		PosTag_feature pf = new PosTag_feature();
 		//add new instance like above example
+		
 		
 		//String newExampleFeature;
 		String CapitalizeFeature;
+		String PostagFeature;
 		
 		//for all the words
 		for(int i = 0 ; i < Constant.ALLDATAS.size() ; i++){
@@ -59,12 +63,20 @@ public class Main {
 			
 			//prevent new line with all white space
 			if(!wordField.isNewLine){
+				//Capitalize
 				cf.setWord(wordField.word);
 				cf.execute();
 				CapitalizeFeature = cf.getFeature().toString();
 				
+				//Postagging
+				pf.setWord(wordField.word);
+				pf.execute();
+				PostagFeature = pf.getPosTag();
+				
 				//setWord and getFeature to get your features
 				Constant.ALLDATAS.get(i).Capitalize = CapitalizeFeature;
+				Constant.ALLDATAS.get(i).PosTag = PostagFeature;
+				
 			} else {
 				
 			}
@@ -78,13 +90,15 @@ public class Main {
 		//String example;
 		//String ner;
 		String capital;
+		String Ptag;
 		for(int i = 0 ; i < Constant.ALLDATAS.size() ; i++){
 			word = Constant.ALLDATAS.get(i).word;
 			//example = Constant.ALLDATAS.get(i).Example;
 			capital = Constant.ALLDATAS.get(i).Capitalize;
+			Ptag = Constant.ALLDATAS.get(i).PosTag;
 			//ner
 			//captial
-			System.out.println(word +" " + capital);
+			System.out.println(word +" " + capital+"         "+Ptag);
 		}
 	}	
 }
