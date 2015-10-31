384e7250bd01e731464aa3e01521425888aa93a9
連振安 <john821216@lianzhenandeMacBook-Air.local>
2015-10-31 17:50:09 +0800

    modify constant

--- src/common/Constant.java
+++ src/common/Constant.java
@@ -9,11 +9,7 @@ public class Constant {
 	public static ArrayList<Field> ALLDATAS = new ArrayList<Field>();
 	
 	public static final String[] OUTPUTFIELDS= {
-<<<<<<< HEAD
-		"word"
-=======
 		"word", "Capitalize" , "PosTag" , "Prep" , "NER"
->>>>>>> 619ea4e4b19301e7b9a602a6d73bf7cd26081abb
 	};
 	
 	//FOR THE LINE WHICH CONTAINS ALL WHITESPACE
